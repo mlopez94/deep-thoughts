3625ce3b6e69f36c12e25109412873000f1e1b67
mlopez <lopezmatthew87@gmail.com>
2021-09-14 00:35:12 -0400

    getting heroku to work

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 // integrate our Apollo server with the Express application as middleware
 async function startServer () {
   await server.start()
